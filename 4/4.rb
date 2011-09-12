34a5e71bc5c4ac510d97abe63fa6c0546b17ae6c
Nick Huber <nicholashuber@gmail.com>
2011-09-12 14:14:40 -0700

    Imroved syntax in 4.rb

--- 4/4.rb
+++ 4/4.rb
@@ -2,14 +2,14 @@ def palindrome?(string)
   string.reverse == string
 end
 
-biggest = 0
+max = 0
 
 for i in 100..999
   for j in 100..999
     if palindrome?((i * j).to_s)
-      biggest = i * j if biggest < i * j
+      max = [max, i * j].max
     end
   end
 end
 
-p biggest
+p max
