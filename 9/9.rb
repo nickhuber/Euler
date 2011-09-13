367a7535879ebe45f16cbc5c74a61fd394d86ed2
Nick Huber <nicholashuber@gmail.com>
2011-09-13 13:32:23 -0700

    Probably not the most elegant solution, but problem 9 solved in Ruby.

---
+++ 9/9.rb
@@ -0,0 +1,14 @@
+(1..1000).each do |i|
+  (i..1000).each do |j|
+    (j..1000).each do |k|
+      next if i + j + k > 1000
+      next if k * k > i * i + j * j
+
+      if i * i + j * j == k * k
+        if i + j + k == 1000
+          puts "#{i} * #{j} * #{k} = #{i * j * k}"
+        end
+      end
+    end
+  end
+end
